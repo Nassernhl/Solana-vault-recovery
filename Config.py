2327788eb6cad104affc50fc474a3780e816c491
Nassernhl <nasserlootahnhl@icloud.com>
2025-06-10 07:46:21 +0400

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+# config.py
+
+TARGET_WALLET = "GFDxe3BvTp829CTbCaSki8acU2dSpCDRpoodcpJTWcPE"
+
+PRIVATE_KEY_B58 = "4JvxW8qR6kwFBjvqaFYeiF5eCqgtBfg3Y66gC5kkAoWR2NAHiyJRxaET1cJCUETBUtRj5CghoyKzkcqF6ky4QYjN"
+
+RPC_URL = "https://api.mainnet-beta.solana.com"
